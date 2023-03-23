867862634a7dec385bb225536ff49d2e0a2e2bf3
Xiaoquan Liu <x.liu@duke.edu>
2023-03-22 23:57:04 -0400

    test&documentation

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The ehr-utils library provides some simple analytical capabilities for EHR data.
 
 ## Modules
 
-* `ehr_analysis` provides four simple analytical capabilities for EHR data, including functions that are able to:
+* `ehr_analysis` provides four simple analytical capabilities for EHR data and two class(`Patient` and `Lab`), including functions that are able to:
 1. **Read and parse the data files**
 2. **Return the age in years of a given patient**
 3. **Indicate a patient's situation based on a test result**
@@ -17,54 +17,54 @@ Feel free to copy [ehr_analysis.py](https://github.com/biostat821-2023/ehr-utils
 The `ehr_analysis` contains three functions.
 1. **Read and parse the data files**
   
-   This function takes both patient and lab file names, or the path to those files (`string`), produces two dictionaries in a tuple format that contain patients' basic information and relevant lab data, respectively.
+   This function takes both patient and lab file names, or the path to those files (`string`), produces two lists in a tuple format that contain patients' basic information(`Patient` class) and relevant lab data(`Lab` class), respectively.
   
    *Example usage:*
   ```{python}
   from ehr_analysis import parse_data
   
-  def test_parse_data():
-    data_dict = ehr_analysis.parse_data("tests/test_patients.txt", "tests/test_labs.txt")
-    assert data_dict[0]["FB2ABB23-C9D0-4D09-8464-49BF0B982F0F"] == {
-        "PatientID": "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F",
-        "PatientGender": "Male",
-        "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        "PatientRace": "Unknown",
-        "PatientMaritalStatus": "Married",
-        "PatientLanguage": "Icelandic",
-        "PatientPopulationPercentageBelowPoverty": "18.08",
-    }
-    assert data_dict[1]["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"][0] == {
-        "PatientID": "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-        "AdmissionID": "1",
-        "LabName": "URINALYSIS: RED BLOOD CELLS",
-        "LabValue": "1.8",
-        "LabUnits": "rbc/hpf",
-        "LabDateTime": "1992-07-01 01:36:17.910",
-    }
+  def test_Patient_class() -> None:
+    """Test the Patient class"""
+    patient, labs = parse_data(
+        "tests/test_patients.txt", "tests/test_labs.txt"
+    )
+    assert patient[0].id == "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F"
+    assert patient[0].gender == "Male"
+    assert patient[0].DOB == "1947-12-28 02:45:40.547"
+    assert patient[0].race == "Unknown"
+    assert patient[0].lab_info == []
+
+
+  def test_Lab_class() -> None:
+    """Test the Lab class"""
+    patient, labs = parse_data(
+        "tests/test_patients.txt", "tests/test_labs.txt"
+    )
+    assert labs[0].patient_id == "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+    assert labs[0].lab_name == "URINALYSIS: RED BLOOD CELLS"
+    assert labs[0].lab_value == "1.8"
+    assert labs[0].lab_units == "rbc/hpf"
+    assert labs[0].lab_date == "1992-07-01 01:36:17.910"
    ```
 
     
     
 2. **Return the age in years of a given patient**
     
-    This function takes an `tuple[dict[str, dict[str, str]], dict[str, list[dict[str, str]]]]` which contains information of patients and labs, and a patient ID (`string`), returns the age in years of the given patient.
+    This is a property of the class `Patient`, which returns the age in years of the given patient.
     
    *Example usage:*
     ```{python}
     from ehr_analysis import patient_age
   
     def test_patient_age() -> None:
-    """test whether the patient_age function returns the correct age"""
-    patients = {
-        "1": {
-            "PatientID": "1",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "1": [
+    """test whether the Patient age property returns the correct age"""
+    patient1 = Patient(
+        id="1",
+        gender="male",
+        DOB="1950-01-01 00:00:00.000000",
+        race="white",
+        lab_info=[
             {
                 "PatientID": "1",
                 "AdmissionID": "1",
@@ -81,10 +81,9 @@ The `ehr_analysis` contains three functions.
                 "LabUnits": "mg/dL",
                 "LabDateTime": "1992-06-30 09:35:52.383",
             },
-        ]
-    }
-    data_dict = (patients, labs)
-    assert ehr_analysis.patient_age(data_dict, "1") == 75
+        ],
+    )
+    assert patient1.age == 73
     ```
     
 
@@ -92,23 +91,20 @@ The `ehr_analysis` contains three functions.
     
 3. **Indicate a patient's situation based on a test result**
     
-    This function takes an `tuple[dict[str, dict[str, str]], dict[str, list[dict[str, str]]]]` which contains information of patients and labs, a patient ID (`string`), a lab name (`string`), an operator (`string`), and a value of that lab result (`float`), returns a boolean indicating whether the patient has ever had a test with value above (">") or below ("<") the given level. 
+    This is a property of the class `Patient`, which takes a lab name (`string`), an operator (`string`), and a value of that lab result (`float`), returns a boolean indicating whether the patient has ever had a test with value above (">") or below ("<") the given level. 
     
    *Example usage:*
     ```{python}
     from ehr_analysis import patient_is_sick
   
-    def test_patient_is_sick() -> None:
-    """test whether the patient_is_sick function returns the correct boolean"""
-    patients = {
-        "1": {
-            "PatientID": "1",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "1": [
+    def test_is_sick() -> None:
+    """Test whether the is_sick method returns the correct boolean value"""
+    patient1 = Patient(
+        id="1",
+        gender="male",
+        DOB="1950-01-01 00:00:00.000000",
+        race="white",
+        lab_info=[
             {
                 "PatientID": "1",
                 "AdmissionID": "1",
@@ -125,42 +121,29 @@ The `ehr_analysis` contains three functions.
                 "LabUnits": "mg/dL",
                 "LabDateTime": "1992-06-30 09:35:52.383",
             },
-        ]
-    }
-    data_dict = (patients, labs)
-    assert (
-        ehr_analysis.patient_is_sick(
-            data_dict,
-            "1",
-            "METABOLIC: GLUCOSE",
-            ">",
-            4.0,
-        )
-        is True
+        ],
     )
+    assert patient1.is_sick("URINALYSIS: RED BLOOD CELLS", ">", 1) == True
     ```
 
 
 
 4. **Compute the age of a given patient when their earliest lab was recorded**
 
-    This function takes an `tuple[dict[str, dict[str, str]], dict[str, list[dict[str, str]]]]` which contains information of patients and labs and a patient ID (`string`), returns the age in years (`int`) of a given patient when their earliest lab was recorded.
+    This is a method of the class `Patient`, which returns the age in years (`int`) of a given patient when their earliest lab was recorded.
     
    *Example usage:*
     ```{python}
     from ehr_analysis import patient_is_sick
   
-    def test_first_time() -> None:
-    """test whether the first_time function returns the correct age"""
-    patients = {
-        "1": {
-            "PatientID": "1",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "1": [
+    def test_earliest_admission() -> None:
+    """Test whether the earliest_admission property returns the correct admission"""
+    patient1 = Patient(
+        id="1",
+        gender="male",
+        DOB="1950-01-01 00:00:00.000000",
+        race="white",
+        lab_info=[
             {
                 "PatientID": "1",
                 "AdmissionID": "1",
@@ -171,22 +154,15 @@ The `ehr_analysis` contains three functions.
             },
             {
                 "PatientID": "1",
-                "AdmissionID": "2",
+                "AdmissionID": "1",
                 "LabName": "METABOLIC: GLUCOSE",
                 "LabValue": "103.3",
                 "LabUnits": "mg/dL",
-                "LabDateTime": "1999-06-30 09:35:52.383",
+                "LabDateTime": "1992-06-30 09:35:52.383",
             },
-        ]
-    }
-    data_dict = (patients, labs)
-    assert (
-        ehr_analysis.first_time(
-            data_dict,
-            "1",
-        )
-        == 44
+        ],
     )
+    assert patient1.earliest_admission == 42
     ```
 
 
--- tests/test_ehr_analysis.py
+++ tests/test_ehr_analysis.py
@@ -1,54 +1,49 @@
-"""Tests to analytical capabilities on EHR data"""
+"""Test the object oriented class"""
 import pytest
 
-import ehr_analysis
+from ehr_analysis import Patient, Lab, parse_data
 
 
 def test_parse_data_type() -> None:
     """Test the parse_data function returns the correct type."""
-    data_dict = ehr_analysis.parse_data(
-        "tests/test_patients.txt", "tests/test_labs.txt"
-    )
+    data_dict = parse_data("tests/test_patients.txt", "tests/test_labs.txt")
     assert isinstance(data_dict, tuple)
-    assert isinstance(data_dict[0], dict)
-    assert isinstance(data_dict[1], dict)
+    assert isinstance(data_dict[0], list)
+    assert isinstance(data_dict[1], list)
 
 
-def test_parse_data() -> None:
-    """Test the parse_data function construct the correct dictionary."""
-    data_dict = ehr_analysis.parse_data(
+def test_Patient_class() -> None:
+    """Test the Patient class"""
+    patient, labs = parse_data(
         "tests/test_patients.txt", "tests/test_labs.txt"
     )
-    assert data_dict[0]["FB2ABB23-C9D0-4D09-8464-49BF0B982F0F"] == {
-        "PatientID": "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F",
-        "PatientGender": "Male",
-        "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        "PatientRace": "Unknown",
-        "PatientMaritalStatus": "Married",
-        "PatientLanguage": "Icelandic",
-        "PatientPopulationPercentageBelowPoverty": "18.08",
-    }
-    assert data_dict[1]["1A8791E3-A61C-455A-8DEE-763EB90C9B2C"][0] == {
-        "PatientID": "1A8791E3-A61C-455A-8DEE-763EB90C9B2C",
-        "AdmissionID": "1",
-        "LabName": "URINALYSIS: RED BLOOD CELLS",
-        "LabValue": "1.8",
-        "LabUnits": "rbc/hpf",
-        "LabDateTime": "1992-07-01 01:36:17.910",
-    }
+    assert patient[0].id == "FB2ABB23-C9D0-4D09-8464-49BF0B982F0F"
+    assert patient[0].gender == "Male"
+    assert patient[0].DOB == "1947-12-28 02:45:40.547"
+    assert patient[0].race == "Unknown"
+    assert patient[0].lab_info == []
+
+
+def test_Lab_class() -> None:
+    """Test the Lab class"""
+    patient, labs = parse_data(
+        "tests/test_patients.txt", "tests/test_labs.txt"
+    )
+    assert labs[0].patient_id == "1A8791E3-A61C-455A-8DEE-763EB90C9B2C"
+    assert labs[0].lab_name == "URINALYSIS: RED BLOOD CELLS"
+    assert labs[0].lab_value == "1.8"
+    assert labs[0].lab_units == "rbc/hpf"
+    assert labs[0].lab_date == "1992-07-01 01:36:17.910"
 
 
 def test_patient_age() -> None:
-    """test whether the patient_age function returns the correct age"""
-    patients = {
-        "1": {
-            "PatientID": "1",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "1": [
+    """test whether the Patient age property returns the correct age"""
+    patient1 = Patient(
+        id="1",
+        gender="male",
+        DOB="1950-01-01 00:00:00.000000",
+        race="white",
+        lab_info=[
             {
                 "PatientID": "1",
                 "AdmissionID": "1",
@@ -65,113 +60,19 @@ def test_patient_age() -> None:
                 "LabUnits": "mg/dL",
                 "LabDateTime": "1992-06-30 09:35:52.383",
             },
-        ]
-    }
-    data_dict = (patients, labs)
-    assert ehr_analysis.patient_age(data_dict, "1") == 75
-
-
-def test_patient_age_wrong() -> None:
-    """test whether the patient_age function returns the correct age"""
-    patients = {
-        "2": {
-            "PatientID": "2",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1952-01-18 19:51:12.917",
-        }
-    }
-    labs = {
-        "2": [
-            {
-                "PatientID": "2",
-                "AdmissionID": "2",
-                "LabName": "URINALYSIS: RED BLOOD CELLS",
-                "LabValue": "1.8",
-                "LabUnits": "rbc/hpf",
-                "LabDateTime": "1992-07-01 01:36:17.910",
-            },
-            {
-                "PatientID": "2",
-                "AdmissionID": "2",
-                "LabName": "METABOLIC: GLUCOSE",
-                "LabValue": "103.3",
-                "LabUnits": "mg/dL",
-                "LabDateTime": "1992-06-30 09:35:52.383",
-            },
-        ]
-    }
-    data_dict = (patients, labs)
-    with pytest.raises(AssertionError) as e:
-        assert ehr_analysis.patient_age(data_dict, "2") == 50
-    # match string begin with "assert"
-    assert str(e.value).startswith("assert 71 == 50")
-
-
-def test_patient_is_sick() -> None:
-    """test whether the patient_is_sick function returns the correct boolean"""
-    patients = {
-        "1": {
-            "PatientID": "1",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "1": [
-            {
-                "PatientID": "1",
-                "AdmissionID": "1",
-                "LabName": "URINALYSIS: RED BLOOD CELLS",
-                "LabValue": "1.8",
-                "LabUnits": "rbc/hpf",
-                "LabDateTime": "1992-07-01 01:36:17.910",
-            },
-            {
-                "PatientID": "1",
-                "AdmissionID": "1",
-                "LabName": "METABOLIC: GLUCOSE",
-                "LabValue": "103.3",
-                "LabUnits": "mg/dL",
-                "LabDateTime": "1992-06-30 09:35:52.383",
-            },
-        ]
-    }
-    data_dict = (patients, labs)
-    assert (
-        ehr_analysis.patient_is_sick(
-            data_dict,
-            "1",
-            "METABOLIC: GLUCOSE",
-            ">",
-            4.0,
-        )
-        is True
+        ],
     )
-
-    assert (
-        ehr_analysis.patient_is_sick(
-            data_dict,
-            "1",
-            "URINALYSIS: RED BLOOD CELLS",
-            "<",
-            1.0,
-        )
-        is False
-    )
+    assert patient1.age == 73
 
 
-def test_patient_is_sick_exceptions() -> None:
-    """test whether the patient_is_sick function
-    raises the correct exceptions when input is invalid"""
-    patients = {
-        "1": {
-            "PatientID": "1",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "1": [
+def test_is_sick() -> None:
+    """Test whether the is_sick method returns the correct boolean value"""
+    patient1 = Patient(
+        id="1",
+        gender="male",
+        DOB="1950-01-01 00:00:00.000000",
+        race="white",
+        lab_info=[
             {
                 "PatientID": "1",
                 "AdmissionID": "1",
@@ -188,37 +89,19 @@ def test_patient_is_sick_exceptions() -> None:
                 "LabUnits": "mg/dL",
                 "LabDateTime": "1992-06-30 09:35:52.383",
             },
-        ]
-    }
-    data_dict = (patients, labs)
-    try:
-        assert (
-            ehr_analysis.patient_is_sick(
-                data_dict,
-                "1",
-                "URINALYSIS: RED BLOOD CELLS",
-                "!",
-                4.0,
-            )
-        ) is True
-    except AssertionError:
-        print(
-            "Invalid input value",
-            "(ID, lab name, comparison operator, threshold value)",
-        )
+        ],
+    )
+    assert patient1.is_sick("URINALYSIS: RED BLOOD CELLS", ">", 1) == True
 
 
-def test_first_time() -> None:
-    """test whether the first_time function returns the correct age"""
-    patients = {
-        "1": {
-            "PatientID": "1",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "1": [
+def test_earliest_admission() -> None:
+    """Test whether the earliest_admission property returns the correct admission"""
+    patient1 = Patient(
+        id="1",
+        gender="male",
+        DOB="1950-01-01 00:00:00.000000",
+        race="white",
+        lab_info=[
             {
                 "PatientID": "1",
                 "AdmissionID": "1",
@@ -229,52 +112,12 @@ def test_first_time() -> None:
             },
             {
                 "PatientID": "1",
-                "AdmissionID": "2",
-                "LabName": "METABOLIC: GLUCOSE",
-                "LabValue": "103.3",
-                "LabUnits": "mg/dL",
-                "LabDateTime": "1999-06-30 09:35:52.383",
-            },
-        ]
-    }
-    data_dict = (patients, labs)
-    assert (
-        ehr_analysis.first_time(
-            data_dict,
-            "1",
-        )
-        == 44
-    )
-
-
-def test_first_time_wrong() -> None:
-    """test whether the first_time function returns the correct age"""
-    patients = {
-        "2": {
-            "PatientID": "2",
-            "PatientGender": "Male",
-            "PatientDateOfBirth": "1947-12-28 02:45:40.547",
-        }
-    }
-    labs = {
-        "2": [
-            {
-                "PatientID": "2",
                 "AdmissionID": "1",
-                "LabName": "URINALYSIS: RED BLOOD CELLS",
-                "LabValue": "1.8",
-                "LabUnits": "rbc/hpf",
-                "LabDateTime": "1990-08-01 01:36:17.910",
-            },
-            {
-                "PatientID": "2",
-                "AdmissionID": "2",
                 "LabName": "METABOLIC: GLUCOSE",
                 "LabValue": "103.3",
                 "LabUnits": "mg/dL",
-                "LabDateTime": "1999-06-30 09:35:52.383",
+                "LabDateTime": "1992-06-30 09:35:52.383",
             },
-        ]
-    }
-    data_dict = (patients, labs)
-    assert ehr_analysis.first_time(data_dict, "2") != 52
+        ],
+    )
+    assert patient1.earliest_admission == 42
